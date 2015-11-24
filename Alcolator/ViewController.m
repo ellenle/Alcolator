e2febe63b8245a96e62ed7a89780b9823e90f862
Ellen <ellenleis@gmail.com>
2015-11-23 22:40:54 -0800

    checkpoint 19 completed

---
+++ Alcolator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alcolator
+//
+//  Created by Ellen Thuy Le on 11/23/15.
+//  Copyright (c) 2015 Ellen Thuy Le. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
